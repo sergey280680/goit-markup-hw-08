4c1710fa38021754da70c0ad15a978686276784e
sergey280680 <sergey280680h@gmail.com>
2021-03-18 23:23:50 +0200

    index.html

--- js/menu.js
+++ js/menu.js
@@ -9,7 +9,7 @@
 		menuBtnRef.classList.toggle("is-open");
 		menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-		document.body.classList.toggle("modal-open");
+		// document.body.classList.toggle("modal-open");
 		mobileMenuRef.classList.toggle("is-open");
 	});
 })();
