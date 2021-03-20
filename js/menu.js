f18d1fc240ff29e29d889025056e0b358c4f4fcf
sergey280680 <sergey280680h@gmail.com>
2021-03-20 16:40:46 +0200

    mobile first

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,44 @@
+
 (() => {
-	const menuBtnRef = document.querySelector("[data-menu-button]");
-	const mobileMenuRef = document.querySelector("[data-menu]");
+	const mobileMenu = document.querySelector(".js-menu-container");
+	const openMenuBtn = document.querySelector(".js-open-menu");
+	const closeMenuBtn = document.querySelector(".js-close-menu");
 
-	menuBtnRef.addEventListener("click", () => {
-		const expanded =
-			menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+	const toggleMenu = () => {
+		const isMenuOpen =
+			openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+		openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+		mobileMenu.classList.toggle("is-open");
+		
+	};
 
-		menuBtnRef.classList.toggle("is-open");
-		menuBtnRef.setAttribute("aria-expanded", !expanded);
+	openMenuBtn.addEventListener("click", toggleMenu);
+	closeMenuBtn.addEventListener("click", toggleMenu);
 
-		// document.body.classList.toggle("modal-open");
-		mobileMenuRef.classList.toggle("is-open");
+	// Закрываем мобильное меню на более широких экранах
+	// в случае изменения ориентации устройства.
+	window.matchMedia("(min-width: 600px)").addEventListener("change", (e) => {
+		if (!e.matches) return;
+		mobileMenu.classList.remove("is-open");
+		openMenuBtn.setAttribute("aria-expanded", false);
 	});
 })();
+
+
+
+
+// (() => {
+// 	const menuBtnRef = document.querySelector("[data-menu-button]");
+// 	const mobileMenuRef = document.querySelector("[data-menu]");
+
+// 	menuBtnRef.addEventListener("click", () => {
+// 		const expanded =
+// 			menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+// 		menuBtnRef.classList.toggle("is-open");
+// 		menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+// 		// document.body.classList.toggle("modal-open");
+// 		mobileMenuRef.classList.toggle("is-open");
+// 	});
+// })();
--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector('[data-modal-open]'),
-        closeModalBtn: document.querySelector('[data-modal-close]'),
-        modal: document.querySelector('[data-modal]'),
-    };
+	const refs = {
+		openModalBtn: document.querySelector("[data-modal-open]"),
+		closeModalBtn: document.querySelector("[data-modal-close]"),
+		backdrop: document.querySelector("[data-backdrop]"),
+	};
 
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
 
 	function toggleModal() {
 		document.body.classList.toggle("modal-open");
-        refs.modal.classList.toggle('backdrop__hidden');
-    }
+		refs.backdrop.classList.toggle("backdrop__hidden");
+	}
 })();
